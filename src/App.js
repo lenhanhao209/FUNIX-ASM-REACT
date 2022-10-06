2d7039f3d8430e947536aec4ad467337a9cdfd55
Tran Le Nhan Hao <haotlnfx16060@funix.edu.vn>
2022-10-06 15:16:43 +0700

    ASM1-update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import StaffList from "./components/StaffListComponent.js";
-import { STAFFS } from "./shared/constants";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 //Declaring App
 const App = () => {
-  const [staffs, setStaffs] = useState(STAFFS);
   return (
     //Display Navbar and staffs when click
     <div>
@@ -16,7 +14,7 @@ const App = () => {
           <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
         </div>
       </Navbar>
-      <StaffList staffs={staffs} />
+      <StaffList />
     </div>
   );
 };
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Card, CardImg, CardTitle, CardBody, CardText } from "reactstrap";
 import dateFormat from "dateformat";
+import { STAFFS } from "../shared/constants";
 
 //Declaring StaffList
-const StaffList = (props) => {
+const StaffList = () => {
+  const [staffs, setStaffs] = useState(STAFFS);
   const [selectedStaff, setSelectedStaff] = useState(null);
   const [columDefault, setColumDefault] = useState(
     "col-12 col-md-6 col-lg-4 mt-3"
@@ -96,7 +98,7 @@ const StaffList = (props) => {
           justifyContent: "center",
         }}
       >
-        {props.staffs.map((staff) => (
+        {staffs.map((staff) => (
           <Card
             className={columDefault}
             onClick={() => setSelectedStaff(staff)}
